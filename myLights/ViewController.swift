779602599674853ed7d0da4d5c5ba1a33a279033
Roman <vazhnik.roma@mail.ru>
2019-07-23 10:36:13 +0300

    Initial Commit

---
+++ myLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myLights
+//
+//  Created by Роман Важник on 23.07.2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
