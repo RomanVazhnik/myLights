b03f7c322d37ac66392ebd1510bcbc25a2c0b29b
Roman <vazhnik.roma@mail.ru>
2019-07-23 16:55:28 +0300

    fixed mistakes

--- myLights/ViewController.swift
+++ myLights/ViewController.swift
@@ -13,49 +13,55 @@ class ViewController: UIViewController {
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
+    
     @IBOutlet var myButton: UIButton!
+    
+    let lightOnAlpha: CGFloat = 1
+    let lightOffAlpha: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         settingToButton() // настройка вида myButton
         self.view.backgroundColor = .black
+        redView.alpha = lightOffAlpha
+        yellowView.alpha = lightOffAlpha
+        greenView.alpha = lightOffAlpha
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         let radius = redView.frame.size.width / 2 // радиус для округления, т.к в viewDidLoad я бы получил размер оригинала
         settingToViews(redView, UIColor.red, radius) // RedView
         settingToViews(yellowView, UIColor.yellow, radius) // YellowView
         settingToViews(greenView, UIColor.green, radius) //GreenView
     }
     
-    //Mark: - action кнопки
+    //MARK: - action кнопки
     @IBAction func buttonPressed() {
         if myButton.titleLabel?.text == "Start" {
             myButton.setTitle("Next", for: .normal)
-            redView.alpha = 1
+            redView.alpha = lightOnAlpha
         } else {
-            if redView.alpha == 1 {
-                redView.alpha = 0.3
-                yellowView.alpha = 1
-            } else if yellowView.alpha == 1 {
-                yellowView.alpha = 0.3
-                greenView.alpha = 1
-            } else if greenView.alpha == 1 {
-                greenView.alpha = 0.3
-                redView.alpha = 1
+            if redView.alpha == lightOnAlpha {
+                redView.alpha = lightOffAlpha
+                yellowView.alpha = lightOnAlpha
+            } else if yellowView.alpha == lightOnAlpha {
+                yellowView.alpha = lightOffAlpha
+                greenView.alpha = lightOnAlpha
+            } else if greenView.alpha == lightOnAlpha {
+                greenView.alpha = lightOffAlpha
+                redView.alpha = lightOnAlpha
             }
         }
     }
     
-    //Mark: - первоначальные настроки для views
+    //MARK: - первоначальные настроки для views
     private func settingToViews(_ view: UIView, _ color: UIColor, _ radius: CGFloat) {
         view.layer.cornerRadius = radius
         view.backgroundColor = color
-        view.alpha = 0.3
     }
     
-    //Mark: - первоначальные настройки для myButton
+    //MARK: - первоначальные настройки для myButton
     private func settingToButton() {
         myButton.backgroundColor = .red
         myButton.tintColor = .white
